5eecb918f76863af4959cb122c7cbde76bcb84d4
Purpud <jenia.kravchuk223@gmail.com>
2022-06-15 12:54:39 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useMemo, useRef, useState } from "react";
+import PostList from "./components/PostList";
+import "./styles/App.css";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+
+function App() {
+  console.log("Привіт");
+
+  const [posts, setPosts] = useState([
+    { id: 1, title: "аа", body: "бб" },
+    { id: 2, title: "гг", body: "аа" },
+    { id: 3, title: "вв", body: "яя" },
+  ]);
+
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+
+  const removePost = (post) => {
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [
+        ...posts.sort((a, b) => a[filter.sort].localeCompare(b[filter.sort])),
+      ];
+    }
+    return posts;
+  }, [filter.sort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [sortedPosts, filter.query]);
+
+  return (
+    <div className="App">
+      <MyModal>fdf</MyModal>
+      <PostForm create={createPost} />
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="Посты про JS"
+      />
+    </div>
+  );
+}
+
+export default App;
